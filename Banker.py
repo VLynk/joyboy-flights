f52163e4f1e1027bc9249988383fac52858c7c79
VLynk <107424278+VLynk@users.noreply.github.com>
2022-11-08 23:12:28 +0530

    Updated Banker and Ticket
    -Banker now verifies card details
    -Ticket now displays more card details on ticket()

--- Banker.py
+++ Banker.py
@@ -3,12 +3,55 @@ import UAM
 import random
 
 def Card():
-    cardNo = input("Enter your card no +> ")
-    cvv = input("Enter CVV +> ")
-    expDate = input("Enter expiry date +> ")
+    cardNo = input("Enter your card no, ie (1234 5678 9012 3456) \n +> ")
+    cvv = input("Enter CVV, ie (123) \n +> ")
+    expDate = input("Enter expiry date, ie (mm/yy) \n +> ")
+    cardlist = []
 
-    if random.randrange(0, 100) < 55:
-        print("Card accepted! Printing Reciept")
+    cvv = int(cvv)
+
+    for i in cardNo.split():
+        for j in i.split():
+            cardlist.append(int(j))
+
+    if CardVerification(cardlist, cvv, expDate) == "verification complete":
+        return "Card is verified", cardNo, expDate
     else:
-        print("Card Declined! Try Again")
-        Ticket.Ticketer(UAM.userLoginName)
+        return "Card is Invalid" 
+
+def CardVerification(cardList, cvv : int, expDate : str):
+    cardSum = 0
+    dateValues = []
+    cardState = False
+    cvvState = False
+    expdateState = False
+
+
+    for k in cardList:
+        cardSum += k
+
+    if len(str(cvv).split()) == 3:
+        cvvState = True
+    else:
+        cvvState = True
+
+    for i in expDate.split("/"):
+        dateValues.append(i)
+
+    if int(dateValues[0]) in [1, 2, 3, 4, 5, 6, 7, 8 ,9 ,10, 11, 12]:
+        if int(dateValues[1]) <= 27 and int(dateValues[1]) >= 23:
+            expdateState = True
+        else:
+            expdateState = False 
+    else:
+        expdateState = False
+
+    if cardSum % 10 == 0:
+        cardState = True
+    else:
+        cardState = False
+
+    if cardState == True and cvvState == True and expdateState == True:
+        return "verification complete"
+    else:
+        return "verfication failed"
--- Ticket.py
+++ Ticket.py
@@ -7,15 +7,13 @@ import time
 def Ticketer(currentUser: str):
     print("+====+ Billing +====+")
     chosenFlight = input("Enter the flight number of your chosen flight +> ")
-    paymentMethod = "CARD"
-        
-    Banker.Card()
-    Receipt(paymentMethod, chosenFlight)
+    cardDetails = Banker.Card()
+    Receipt(cardDetails[1], cardDetails[2], chosenFlight)
 
-def Receipt(paymentMethod: str, chosenFlightNumber: str):
+def Receipt(cardNo : str, cardExpdate : str, chosenFlightNumber: str):
     print("#######################################################")
-    currentTime = datetime.now()
-    print(f'Time of Purchase : {currentTime} \nPayment Status : Paid \nPaymentMethod : {paymentMethod} \nChosen Flight : {chosenFlightNumber}')
+    currentTime = datetime.today()
+    print(f'Time of Purchase : {currentTime} \nPayment Status : Paid \nPaymentMethod : Card \nCard Number : {cardNo} \nCard Expiry date : {cardExpdate} \nChosen Flight : {chosenFlightNumber}')
     print("######################################################")
     time.sleep(7)
     TaFControl.Hub()
